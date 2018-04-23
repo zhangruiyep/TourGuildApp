96afc91444da841e48f6c999d89f2230540c982b
zhangruiyep <zhangruiyep@outlook.com>
2018-04-23 19:26:04 +0800

    add item and style

--- app/src/main/java/com/example/android/tourguildapp/InfoAdapter.java
+++ app/src/main/java/com/example/android/tourguildapp/InfoAdapter.java
@@ -1,13 +1,49 @@
 package com.example.android.tourguildapp;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class InfoAdapter extends ArrayAdapter<TSInfo> {
+    private int mColor;
 
-    public InfoAdapter(Activity context, ArrayList<TSInfo> InfoList) {
+    public InfoAdapter(Activity context, ArrayList<TSInfo> InfoList, int colorID) {
         super(context, 0, InfoList);
+        mColor = colorID;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+        final TSInfo curInfo = getItem(position);
+
+        TextView titleTextView = listItemView.findViewById(R.id.title_text_view);
+        titleTextView.setText(curInfo.getmTitle());
+
+        TextView detailTextView = listItemView.findViewById(R.id.detail_text_view);
+        detailTextView.setText(curInfo.getmInfo());
+
+        ImageView imageView = listItemView.findViewById(R.id.image_view);
+        imageView.setImageResource(curInfo.getmImgRes());
+
+        LinearLayout itemLayout = listItemView.findViewById(R.id.item_layout);
+        itemLayout.setBackgroundResource(mColor);
+
+        return listItemView;
     }
 }
--- app/src/main/java/com/example/android/tourguildapp/TouristSpotsActivity.java
+++ app/src/main/java/com/example/android/tourguildapp/TouristSpotsActivity.java
@@ -2,6 +2,9 @@ package com.example.android.tourguildapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TouristSpotsActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class TouristSpotsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_list);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ArrayList<TSInfo> info = new ArrayList<TSInfo>();
+        info.add(new TSInfo(R.drawable.bell_tower, "钟楼", "莲湖区东大街和西大街交汇处"));
+        info.add(new TSInfo(R.drawable.gulou, "鼓楼","北院门74号"));
+
+
+        //InfoAdapter itemsAdapter = new InfoAdapter(this, info, R.color.category_colors);
+        InfoAdapter itemsAdapter = new InfoAdapter(this, info, R.color.colorPrimaryDark);
+        ListView listView = (ListView) findViewById(R.id.info_list_view);
+        //GridView listView = (GridView) findViewById(R.id.listView);
+        listView.setAdapter(itemsAdapter);
+
     }
 }
